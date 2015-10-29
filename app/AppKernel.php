b97feef20f9b8c6de50753d7a42d06cc5d3cf975
RickySSD <ricky6982@gmail.com>
2015-10-29 16:56:15 -0300

    Se creo el bundle para el frontend.
    Signed-off-by: RickySSD <ricky6982@gmail.com>

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Tesis\AdminBundle\AdminBundle(),
+            new Tesis\FrontendBundle\FrontendBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {

9698d1cec3d3114cd9075146e1ac6fef3cabc281
RickySSD <ricky6982@gmail.com>
2015-11-15 19:23:11 -0300

    Mostrando al usuario logueado, se agrego nombre a la vista de local comercial.
    Signed-off-by: RickySSD <ricky6982@gmail.com>

--- src/Tesis/AdminBundle/Controller/LocalComercialController.php
+++ src/Tesis/AdminBundle/Controller/LocalComercialController.php
@@ -15,11 +15,15 @@ class LocalComercialController extends Controller
     public function indexAction(Request $request)
     {
 
-        $localForm = $this->createForm(new LocalComercialType());
+        $user = $this->getUser();
+        $em = $this->getDoctrine()->getManager();
+        $local = $em->getRepository('AdminBundle:Usuario')->findOneByUsuario($user->getUsername());
+        $localForm = $this->createForm(new LocalComercialType(), $local);
         $categoriaForm = $this->createForm(new CategoriaType());
         $productoForm = $this->createForm(new ProductoType());
 
         return $this->render('AdminBundle:LocalComercial:index.html.twig', array(
+                'local' => $local,
                 'localForm' => $localForm->createView(),
                 'categoriaForm' => $categoriaForm->createView(),
                 'productoForm' => $productoForm->createView()
@@ -28,6 +32,20 @@ class LocalComercialController extends Controller
 
     public function saveInfoAction(Request $request)
     {
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->getUser();
+        $local = $em->getRepository('AdminBundle:Usuario')->findOneByUsuario($user->getUsername());
+        $localForm = $this->createForm(new LocalComercialType(), $local);
+        $localForm->handleRequest($request);
+
+        if ($localForm->isValid()) {
+            $em->persist($local);
+            $em->flush();
+            $this->get('session')->getFlashBag()->add('success', 'Se guardaron correctamente los datos.');
+        }else{
+            $this->get('session')->getFlashBag()->add('error', 'Se produjo un error al querer guardar los datos, por favor intente nuevamente.');
+        }
+
         return $this->redirectToRoute('local_homepage');
     }
 

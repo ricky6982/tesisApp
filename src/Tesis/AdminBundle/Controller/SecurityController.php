64f868f6cc57791add79a5c23bafda52c31eef32
RickySSD <ricky6982@gmail.com>
2015-11-24 23:34:45 -0300

    Se soluciono el mensaje de error
    En la sección de login de usuario aparecia un error cuando el usuario no se logueaba carrectamente.
    
    Signed-off-by: RickySSD <ricky6982@gmail.com>

--- src/Tesis/AdminBundle/Controller/SecurityController.php
+++ src/Tesis/AdminBundle/Controller/SecurityController.php
@@ -9,6 +9,9 @@ class SecurityController extends Controller
     public function loginAction()
     {
         $helper = $this->get('security.authentication_utils');
+        if ($helper->getLastAuthenticationError()) {
+            $this->get('session')->getFlashBag()->add('error', 'Usuario y/o Contraseña incorrectos.');
+        }
 
         return $this->render('AdminBundle:Security:index.html.twig', array(
                 'last_username' => $helper->getLastUsername(),
